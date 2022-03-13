ec344045757eebf0541115f31cf8734fd5317bdb
NusaibaNizam <53972786+NusaibaNizam@users.noreply.github.com>
2022-03-13 16:40:35 +0600

    redux

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import AppNavigator from './App/AppNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { store } from './App/Redux/store';
+
+
+
 export default function App() {
   return (
+    <Provider store={store}>
       <NavigationContainer>
         <AppNavigator/>
-
-    </NavigationContainer>
+      </NavigationContainer>
+    </Provider>
   );
 }
---
+++ App/Redux/reducer.js
@@ -0,0 +1,11 @@
+import * as actionTypes from './actionTypes';
+
+const initialState={
+    dishes:[]
+};
+export const reducer=(state=initialState,action)=>{
+    switch(action.type){
+        default:
+            return state;
+    }
+}
